b028e4f54d0464b2f24dc66068f01624e3fb3014
Zachatoo <Zachatoo@users.noreply.github.com>
2024-08-22 20:57:32 -0600

    Use http api instead of unsplash js sdk

--- server.js
+++ server.js
@@ -1,34 +1,33 @@
 import express from "express";
 import cors from "cors";
-import { createApi } from "unsplash-js";
 
-const unsplash = createApi({
-  accessKey: process.env.UNSPLASH_ACCESS_KEY,
-});
 const app = express();
 const port = process.env.PORT || 3000;
+const baseUrl = "https://api.unsplash.com";
 app.use(cors());
 
 app.get("/", async (req, res) => {
   try {
     const { q } = req.query;
-    const obj = {};
-    if (q) {
-      obj.query = q;
-    }
-    const result = await unsplash.photos.getRandom(obj);
+    const searchParams = new URLSearchParams(q);
+    const url = new URL(`/photos/random?${searchParams}`, baseUrl);
+    const response = await fetch(url, {
+      headers: {
+        Authorization: `Client-ID ${process.env.UNSPLASH_ACCESS_KEY}`,
+      },
+    });
 
-    if (result.errors) {
-      res.send(result.errors);
-      return;
+    if (!response.ok) {
+      const error = await response.json();
+      throw error;
     }
-    const photo = result.response;
+    const photo = await response.json();
     const full = photo.urls.full;
     const photog = photo.user.name;
     const md = `![photo by ${photog} on Unsplash](${full})`;
     res.send({ full, photog, md });
   } catch (err) {
-    res.send(err);
+    res.status(500).send(err);
   }
 });
 
